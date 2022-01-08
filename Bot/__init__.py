5855bbd3e18b42ca4017147a5b9c53d18a91b3f4
Anshul Garg <garganshul553@gmail.com>
2022-01-08 23:10:42 +0530

    Create __init__.py

---
+++ Bot/__init__.py
@@ -0,0 +1,89 @@
+#(©)Codexbotz
+
+import pyromod.listen
+from pyrogram import Client
+import sys, os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+API_HASH = '90dd95178a8d13a69bfdbc7da68d23a4'
+APP_ID = 2919867
+TG_BOT_TOKEN = '5049507559:AAETCRZ2YM2VR9fj8DuNIfqUEnxp1HSavoc'
+TG_BOT_WORKERS = 4
+FORCE_SUB_CHANNEL = -1001611440971
+CHANNEL_ID = -1001705686821
+ADMINS = set(int(x) for x in  [5073412581,1392459364,1242979521,1337239251,2043468602,1791795037])
+DISABLE_CHANNEL_BUTTON = True
+DB_URI = "postgres://cstzoubfhqzpdh:32eaf163608c43c99d097b0d8941a052438c8ea75a87d0c34d3af4957dd5ea09@ec2-34-231-56-78.compute-1.amazonaws.com:5432/d80pcqtp3js9bk"
+FORCE_MSG = """
+JOIN OUR CHANNEL TO GET ANIMES 😌 """
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            "Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            plugins={
+                "root": "Bot/plugs"
+            },
+            workers=TG_BOT_WORKERS,
+            bot_token=TG_BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+
+        if FORCE_SUB_CHANNEL:
+            try:
+                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                self.invitelink = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).info("\nBot Stopped")
+                sys.exit()
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped.")
+            sys.exit()
+
+        self.set_parse_mode("html")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by Villians\nhttps://t.me/Villians_association")
+        self.username = usr_bot_me.username
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")

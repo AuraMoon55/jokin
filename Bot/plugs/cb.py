8e3bb332d6f3b7907fe696616cfe8c579fcfb36b
Anshul Garg <garganshul553@gmail.com>
2022-01-09 00:06:30 +0530

    Update cb.py

--- Bot/plugs/cb.py
+++ Bot/plugs/cb.py
@@ -7,24 +7,7 @@ from Bot.helper_func import encode
 from Bot import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from Bot import Pbot as Bot
 
-@Bot.on_message(filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink']))
-async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...!", quote = True)
-    try:
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
-    except Exception as e:
-        print(e)
-        await reply_text.edit_text("Something went Wrong..!")
-        return
-    converted_id = post_message.message_id * abs(client.db_channel.id)
-    string = f"get-{converted_id}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(" Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
@@ -49,7 +32,7 @@ async def new_post(client: Client, message: Message):
         pass
 
 
-@Bot.on_message(filters.chat(USE_GROUP) & filters.user(ADMINS) & filters.command('batch'))
+@Bot.on_message(filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
@@ -83,7 +66,7 @@ async def batch(client: Client, message: Message):
     await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
-@Bot.on_message(filters.chat(USE_GROUP) & filters.user(ADMINS) & filters.command('genlink'))
+@Bot.on_message(filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
